b0654420fb7c9033d6f582efd536ff52b97de023
yulee <dbfl0461@gmail.com>
2023-07-03 16:47:46 +0900

    프로젝트 설정

---
+++ src/main/java/com/example/laweasy/LaweasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.laweasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaweasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaweasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laweasy/LaweasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laweasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaweasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
