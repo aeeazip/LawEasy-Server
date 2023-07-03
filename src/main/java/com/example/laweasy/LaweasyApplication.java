07d11176fa38ec344282a7eeeb4a51a57d1e8e2a
aeeazip <codnjs0809@naver.com>
2023-07-03 20:28:27 +0900

    feat : 로그인, 회원가입 구현

--- src/main/java/com/example/laweasy/LaweasyApplication.java
+++ src/main/java/com/example/laweasy/LaweasyApplication.java
@@ -2,8 +2,10 @@ package com.example.laweasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LaweasyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/laweasy/controller/MemberController.java
+++ src/main/java/com/example/laweasy/controller/MemberController.java
@@ -1,7 +1,72 @@
 package com.example.laweasy.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.laweasy.dto.LoginReqDto;
+import com.example.laweasy.dto.LoginResDto;
+import com.example.laweasy.dto.MemberReqDto;
+import com.example.laweasy.dto.MemberResDto;
+import com.example.laweasy.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.laweasy.config.*;
 
-@Controller
+import static com.example.laweasy.utils.ValidationRegex.isRegexEmail;
+import static com.example.laweasy.utils.ValidationRegex.isRegexPassword;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+    private final MemberService memberService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public BaseResponse<MemberResDto> signup(@RequestBody MemberReqDto memberReqDto) throws BaseException {
+        if (memberReqDto.getEmail() == null)
+            return new BaseResponse<>(BaseResponseStatus.POST_USERS_EMPTY_EMAIL);
+
+        if (!isRegexEmail(memberReqDto.getEmail()))
+            return new BaseResponse<>(BaseResponseStatus.POST_USERS_INVALID_EMAIL);
+
+        if (!isRegexPassword(memberReqDto.getPassword()))
+            return new BaseResponse<>(BaseResponseStatus.POST_USERS_INVALID_PASSWORD);
+
+        String role = memberReqDto.getRole();
+        if(!role.equals("일반") && !role.equals("전문가"))
+            return new BaseResponse<>(BaseResponseStatus.POST_USERS_INVALID_ROLE);
+
+        if(memberReqDto.getNickname() == null)
+            return new BaseResponse<>(BaseResponseStatus.POST_USERS_EMPTY_NICKNAME);
+
+        try {
+            MemberResDto memberResDto = memberService.signup(memberReqDto);
+            return new BaseResponse<>(memberResDto);
+        } catch(BaseException e) {
+            e.printStackTrace();
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public BaseResponse<LoginResDto> login(@RequestBody LoginReqDto loginReqDto) {
+        try {
+            if (loginReqDto.getEmail() == null || loginReqDto.getPassword() == null)
+                return new BaseResponse<>(BaseResponseStatus.FAILED_TO_LOGIN);
+
+            if (!isRegexEmail(loginReqDto.getEmail()))
+                return new BaseResponse<>(BaseResponseStatus.POST_USERS_INVALID_EMAIL);
+
+            if (!isRegexPassword(loginReqDto.getPassword()))
+                return new BaseResponse<>(BaseResponseStatus.POST_USERS_INVALID_PASSWORD);
+
+            LoginResDto loginResDto = memberService.login(loginReqDto);
+            return new BaseResponse<>(loginResDto);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
 }
